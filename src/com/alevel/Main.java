c460263e8a29e687d536f65bcfd37a519d055794
Chizhov98 <chizhov1998a@gmail.com>
2021-05-29 16:49:18 +0300

    hw10 Chizhov Artem

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -5,7 +5,23 @@ public class Main {
     public static void main(String[] args) {
         Report report = Report.builder().header("Header").body("Body").build();
         Report report1 = report.toBuilder().footer("Footer").build();
-
-        System.out.printf("");
+        Pow toPow = (int number, int pow) -> {
+            int newNum = number;
+            if (pow > 0) {
+                for (int i = 1; i < pow; i++) {
+                    newNum *= number;
+                }
+            } else if (pow < 0) {
+                pow = -1 * pow;
+                for (int i = 1; i < pow; i++) {
+                    newNum *= number;
+                }
+                return 1.0 / newNum;
+            } else {
+                return 1;
+            }
+            return (double) newNum;
+        };
+        System.out.printf(String.valueOf(toPow.pow(5, 3)));
     }
 }
