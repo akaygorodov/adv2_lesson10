069d49327f0c3accfb62aa2c170f56c55c80d6d2
Nikita658 <nikita14bykov@gmail.com>
2021-06-01 20:30:02 +0300

    learning lamda and interface

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -1,11 +1,15 @@
 package com.alevel;
 
 public class Main {
-
     public static void main(String[] args) {
-        Report report = Report.builder().header("Header").body("Body").build();
-        Report report1 = report.toBuilder().footer("Footer").build();
 
-        System.out.printf("");
+        Pow pow =(number, pow1) ->{
+            int result =1;
+            for (int i = 0; i <pow1 ; i++) {
+                result *= number;
+            }
+            return result;
+        };
+        System.out.println(pow.pow(2,3));
     }
 }
---
+++ src/com/alevel/Pow.java
@@ -0,0 +1,5 @@
+package com.alevel;
+@FunctionalInterface
+public interface Pow {
+    double pow(int number, int pow);
+}
