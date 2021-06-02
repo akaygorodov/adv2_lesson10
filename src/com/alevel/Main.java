10dcb2b1c515ed6238be9a789ae318fd9496679d
Grandius <gradius777@gmail.com>
2021-06-02 03:07:05 +0300

    Initial commit of HW10

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -3,9 +3,21 @@ package com.alevel;
 public class Main {
 
     public static void main(String[] args) {
+
         Report report = Report.builder().header("Header").body("Body").build();
         Report report1 = report.toBuilder().footer("Footer").build();
+        System.out.println(report);
+        System.out.println(report1);
 
-        System.out.printf("");
+        Pow mathPower = (number, powerValue) -> {
+            int result = 1;
+            while (powerValue>0) {
+                result *= number;
+                powerValue--;
+            }
+            return result;
+        };
+        System.out.println("Math power 4 of number 5 is " + mathPower.calculateMathPower(5, 4));
+
     }
 }
