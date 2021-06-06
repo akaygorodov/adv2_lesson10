e84ae27ab37d77ac93428fc7b98304e20318a0a5
Khalin7494 <khalin@protonmail.com>
2021-06-06 16:22:40 +0300

    Main HW11

---
+++ src/com/alevel/Main_HW11.java
@@ -0,0 +1,14 @@
+package com.alevel;
+
+import java.util.Arrays;
+import static com.alevel.PlanetEnum.*;
+
+public class Main_HW11 {
+    public static void main(String[] args) {
+        System.out.println(MARS.toString());
+
+        // или так
+        /*PlanetEnum planetEnum = PlanetEnum.valueOf("MARS");
+        System.out.println(planetEnum);*/
+    }
+}
