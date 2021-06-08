0ac7537203976db3c5e4e1d3df4434497f770683
TarasovDmytro <tarasov.personal@gmail.com>
2021-06-08 11:37:32 +0300

    HW12 - refactor + try-with-resources

--- hw12/Demo.java
+++ hw12/Demo.java
@@ -3,31 +3,22 @@ package ua.tarasov.adv2.hw12;
 import java.io.*;
 
 public class Demo {
-        public static String read(String path){
-            BufferedReader reader = null;
-            StringBuilder stringBuilder = null;
-//открываем файл по пути
-            try {
-                reader = new BufferedReader( new FileReader(path));
-                stringBuilder = new StringBuilder();
-                String currentString;
+        public static String read(String path) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String currentString;
+
+//открываем файл по пути и, после использования, автоматически закрываем
+            try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
 //построчно считываем файл
-                while ((currentString = reader.readLine()) != null ){
+                while ((currentString = reader.readLine()) != null) {
                     stringBuilder.append(currentString);
-                    stringBuilder.append( "\n" );
+                    stringBuilder.append("\n");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-//закрываем файл
-                try {
-                    assert reader != null;
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 //возвращаем вычитанный текст в строке
-                assert stringBuilder != null;
                 return stringBuilder.toString();
             }
 
