803f6c87974737e278387c2a2971b82970a838fa
Nikita658 <nikita14bykov@gmail.com>
2021-06-09 20:05:24 +0300

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Demo {
+    public static String read(String path) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            StringBuilder stringBuilder = new StringBuilder();
+            String currentString;
+
+            while ((currentString = reader.readLine()) != null) {
+                stringBuilder.append(currentString);
+                stringBuilder.append(" \n ");
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            System.out.println("error in specifying the path! " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        System.out.println(read("C:\\test.txt.txt"));
+    }
+}
